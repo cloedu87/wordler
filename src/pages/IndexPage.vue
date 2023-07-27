20fb45149dc2d8178f0899ac58fe717cc1cbb7b0
claudios <claudio.siegenthaler@zebbra.ch>
2023-07-27 13:41:34 +0200

    some first impressions

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,44 +1,18 @@
 <template>
   <q-page class="row items-center justify-evenly">
-    <example-component
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></example-component>
+    verben: {{ verben }}
   </q-page>
 </template>
 
 <script setup lang="ts">
-import { Todo, Meta } from 'components/models';
-import ExampleComponent from 'components/ExampleComponent.vue';
-import { ref } from 'vue';
+import { computed } from 'vue';
+import { useSheetStore } from '../stores/sheets';
 
 // https://docs.google.com/spreadsheets/d/1cz4SAePV011rWPZQ6IFfYfmNftpgXDefkmwABzp9s2k/edit?usp=sharing
 
-const todos = ref<Todo[]>([
-  {
-    id: 1,
-    content: 'ct1',
-  },
-  {
-    id: 2,
-    content: 'ct2',
-  },
-  {
-    id: 3,
-    content: 'ct3',
-  },
-  {
-    id: 4,
-    content: 'ct4',
-  },
-  {
-    id: 5,
-    content: 'ct5',
-  },
-]);
-const meta = ref<Meta>({
-  totalCount: 1200,
-});
+const sheetsStore = useSheetStore();
+
+sheetsStore.fetchSheet();
+
+const verben = computed(() => sheetsStore.verben);
 </script>
---
+++ src/stores/sheets.ts
@@ -0,0 +1,38 @@
+import axios from 'axios';
+import { defineStore } from 'pinia';
+
+export interface SheetState {
+  verben: any[];
+  nomen: any[];
+  adjektive: any[];
+}
+
+export const useSheetStore = defineStore('sheets', {
+  state: (): SheetState => ({
+    verben: [],
+    nomen: [],
+    adjektive: [],
+  }),
+  getters: {
+    getVerben: (state) => state.verben,
+  },
+  actions: {
+    async fetchSheet() {
+      const sheetResponse = await axios.get(
+        'https://api.sheetson.com/v2/sheets/verben',
+        {
+          method: 'POST',
+          headers: {
+            Authorization:
+              'Bearer 0Q9pwKzbP6622ESVJ7Lzp8blNde1j9ynjgP1Uc5sp1XZGL4UnWWOC2zCORU',
+            'X-Spreadsheet-Id': '1cz4SAePV011rWPZQ6IFfYfmNftpgXDefkmwABzp9s2k',
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+      console.log(sheetResponse.data.results);
+
+      this.verben = sheetResponse.data.results;
+    },
+  },
+});
